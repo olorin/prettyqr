132a85367ef8a91f026a70cc9ef1fce133e4464c
Sharif Olorin <sio@tesser.org>
2016-07-02 08:54:12 +0000

    Alpha-composite QR image onto base image

--- prettyqr/pretty.py
+++ prettyqr/pretty.py
@@ -11,10 +11,15 @@ def load_image(fname):
     im_ = im.convert("RGBA")
     return im_
 
-def make_pretty_qr(data, img, border=4, box_size=10, out_path="qr.png"):
-    base_img = load_image(img)
-    base_img_size = base_img.size[0]
+# FIXME: slow
+def rgba_search_replace(img, old, new):
+    for x in range(0, img.size[0]):
+        for y in range(0, img.size[1]):
+            px = img.getpixel((x, y))
+            if px == old:
+                img.putpixel((x, y), new)
 
+def make_qr(data, box_size, border, width, height):
     qr = qrcode.QRCode(
         version=None,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
@@ -23,21 +28,26 @@ def make_pretty_qr(data, img, border=4, box_size=10, out_path="qr.png"):
     )
     qr.add_data(data)
     qr.make(fit=True)
-    qr_img = qr.make_image()
-
+    qr_img = qr.make_image().convert("RGBA")
+    rgba_search_replace(qr_img, (255, 255, 255, 255), (0, 0, 0, 0))
     border_px = border * box_size
-    qr_w = base_img_size - (border_px * 2)
-    qr_h = base_img_size - (border_px * 2)
-
+    qr_w = width - (border_px * 2)
+    qr_h = height - (border_px * 2)
     qr_scaled = qr_img.resize((qr_w, qr_h))
-
+    qr_layer = Image.new("RGBA", (width, height), color=(0, 0, 0, 0))
     qr_box = (
         border_px,
         border_px,
-        base_img_size - border_px,
-        base_img_size - border_px
+        height - border_px,
+        width - border_px
     )
-    base_img.paste(qr_scaled, qr_box)
+    qr_layer.paste(qr_scaled, qr_box)
+    return qr_layer
 
-    base_img.save(out_path)
+def make_pretty_qr(data, img, border=4, box_size=10, out_path="qr.png"):
+    base_img = load_image(img)
+    qr_img = make_qr(data, box_size, border, base_img.size[0], base_img.size[1])
+    final_img = Image.alpha_composite(base_img, qr_img)
+
+    final_img.save(out_path)
     
