978a4ad86802eb22e5c56ea53e8839481b873a9b
Sharif Olorin <sio@tesser.org>
2016-07-02 08:09:28 +0000

    Naively paste QR code in the middle of the base image

---
+++ prettyqr/__init__.py
@@ -0,0 +1,1 @@
+from .pretty import make_pretty_qr
--- prettyqr/pretty.py
+++ prettyqr/pretty.py
@@ -8,19 +8,36 @@ def load_image(fname):
     (w, h) = im.size
     if w != h:
         raise ImageNotSquareException
-    return im
+    im_ = im.convert("RGBA")
+    return im_
 
-def make_pretty_qr(data, img, out_path="qr.png"):
-    backing_img = load_image(img)
+def make_pretty_qr(data, img, border=4, box_size=10, out_path="qr.png"):
+    base_img = load_image(img)
+    base_img_size = base_img.size[0]
+
     qr = qrcode.QRCode(
         version=None,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=10,
-        border=4,
+        box_size=box_size,
+        border=0, # we make our own border
     )
     qr.add_data(data)
     qr.make(fit=True)
     qr_img = qr.make_image()
-    qr_scaled = qr_img.resize(backing_img.size)
-    qr_scaled.save(out_path)
+
+    border_px = border * box_size
+    qr_w = base_img_size - (border_px * 2)
+    qr_h = base_img_size - (border_px * 2)
+
+    qr_scaled = qr_img.resize((qr_w, qr_h))
+
+    qr_box = (
+        border_px,
+        border_px,
+        base_img_size - border_px,
+        base_img_size - border_px
+    )
+    base_img.paste(qr_scaled, qr_box)
+
+    base_img.save(out_path)
     
